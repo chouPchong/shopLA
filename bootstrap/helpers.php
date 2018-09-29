f6470f59ca70ee9e8a26bbf626cebe108d11ddec
zhangtao@allmedia-cn.com <zhangtao@allmedia-cn.com>
2018-09-29 14:29:17 +0800

    辅助函数

---
+++ bootstrap/helpers.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * 功能：自定义辅助函数；
+ * 条件：1、composer.json 文件添加 "file": ["bootstrap/helpers.php"]；2、composer dumpautoload
+ */
+
+function test_helper()
+{
+    return 'ok';
+}
--- resources/views/working_test/location_test.blade.php
+++ resources/views/working_test/location_test.blade.php
@@ -8,13 +8,17 @@
     <title>location_test</title>
 </head>
 <body>
-<span id="lng"></span>,
-<span id="lat"></span>
+当前地理位置：<span id="lng"></span>,<span id="lat"></span><br>
+经度：<input type="text" name="lng"><br>
+纬度：<input type="text" name="lat">
+<button onclick="result()">计算</button>
+<div id="length"></div>
 <div id="iCenter"></div>
-<script type="text/javascript" src="https://webapi.amap.com/maps?v=1.4.10&key=cdd9f5fffb44afdb682d1e68c68c247f"></script>
+<script type="text/javascript"
+        src="https://webapi.amap.com/maps?v=1.4.10&key=cdd9f5fffb44afdb682d1e68c68c247f"></script>
 <script>
     mapObj = new AMap.Map('iCenter');
-    mapObj.plugin('AMap.Geolocation', function() {
+    mapObj.plugin('AMap.Geolocation', function () {
         var geolocation = new AMap.Geolocation({
             // 是否使用高精度定位，默认：true
             enableHighAccuracy: true,
@@ -32,15 +36,28 @@
         AMap.event.addListener(geolocation, 'complete', onComplete);
         AMap.event.addListener(geolocation, 'error', onError);
 
-        function onComplete (data) {
+        function onComplete(data) {
             document.getElementById('lng').innerText = data.position.lng;
             document.getElementById('lat').innerText = data.position.lat;
         }
 
-        function onError (data) {
+        function onError(data) {
             console.log(data)
         }
-    })
+    });
+
+    function result() {
+        var lng0 = document.getElementById('lng').innerText;
+        var lat0 = document.getElementById('lat').innerText;
+        var lng = document.getElementsByName('lng')[0].value;
+        var lat = document.getElementsByName('lat')[0].value;
+        var p1 = [lng0, lat0];
+        var p2 = [lng, lat];
+        console.log(p1);
+        console.log(p2);
+        var dis = AMap.GeometryUtil.distance(p1, p2);
+        document.getElementById('length').innerText = dis;
+    }
 
 </script>
 </body>
