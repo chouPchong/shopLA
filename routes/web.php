f0a194a659dfe0c327462165b9acb7d4369e2130
zhangtao@allmedia-cn.com <zhangtao@allmedia-cn.com>
2018-10-08 15:55:50 +0800

    前台页面-商品详情页

--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,5 @@ Route::group(['middleware' => 'auth'], function() {
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
